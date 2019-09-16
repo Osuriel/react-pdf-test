05e9fe43f8777cd9470a3936f7dec4ee7a740bb8
Omar Suriel <omar@cloverleaf.me>
2019-09-16 15:42:18 -0400

    cleaned it up

--- src/index.js
+++ src/index.js
@@ -1,22 +1,28 @@
 import Koa from 'koa';
 import React from 'react';
-import ReactPDF from '@react-pdf/renderer';
+import ReactPDF, { pdf } from '@react-pdf/renderer';
 import MyDocument from './MyDocument';
 
 
 const app = new Koa();
 
 app.use(async ctx => {
-  console.log('ctx.path: ', ctx.path);
+
+  if(ctx.path === '/saveFile') {
+    await ReactPDF.render(<MyDocument />, `${__dirname}/example.pdf`).then(() => console.log('pdf done'));
+    ctx.body='file generated and saved';
+    return;
+  }
 
   if(ctx.path === '/render') {
-    await ReactPDF.render(<MyDocument />, `${__dirname}/example.pdf`).then(() => console.log('pdf done'));
-    ctx.body = 'PDF rendered';
+    const pdfBuffer = await pdf(<MyDocument />).toBuffer();
+    ctx.set('Content-Type', 'application/pdf');
+    console.log('rendered pdf successfully');
+    ctx.body = pdfBuffer;
+
     return;
   }
   ctx.body='nothing rendered';
 });
 
-console.log('node is up');
-
-app.listen(3050);
+app.listen(3050, () => console.log('listening on port 3050'));
